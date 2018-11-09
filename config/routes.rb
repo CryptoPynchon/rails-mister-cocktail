4b7d0f7b135690d570dab41b8cc8e95910aa1f31
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-09 12:53:06 -0500

    styling on em

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   resources :cocktails, only: [:index, :new, :show, :create]
 
-  get 'cocktails/:cocktail_id/doses/new', to: 'doses#new'
+  get 'cocktails/:cocktail_id/doses/new', to: 'doses#new', as: 'new_doses'
   get 'cocktails/:cocktail_id/doses', to: 'cocktails#show', as: 'doses'
   post 'cocktails/:cocktail_id/doses', to: 'doses#create'
-  delete 'doses/:id', to: 'doses#destroy'
+  delete 'doses/:id', to: 'doses#destroy', as: 'dose'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
